1a72f98265efd4740e73fe5cb2527ad8450a9239
Silvestru17 <silvestru.state17@gmail.com>
2022-06-07 17:30:37 +0300

    49

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,7 +12,7 @@ class ModelTests(TestCase):
     def test_create_user_with_email_succesful(self):
         """Test creating a user with email is succesfull."""
 
-        email = 'test@example.com',
+        email = 'test@example.com'
         password = 'testpass123'
 
         user = get_user_model().objects.create_user(
