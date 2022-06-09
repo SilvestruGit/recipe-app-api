c76532fc5db7bd80c80004daebccc6aa77584063
Silvestru17 <silvestru.state17@gmail.com>
2022-06-09 15:00:59 +0300

    check schema

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_spectacular.views import (
+    SpectacularApiView,
+    SpectacularSwaggerView,
+)
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema', SpectacularApiView.as_view(), name='api-schema'),
+    path(
+        'api/docks',
+        SpectacularSwaggerView.as_view(),
+        name='api-docs',
+    )
 ]
